7aad623aedfc0310ece471b8df51765b08aef74a
LostLuma <lilly@lostluma.net>
2025-04-08 11:01:31 +0200

    Simplify server-side Open Inventory achievement tracking
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- module/versions/1.3.2-to-1.4.0-alpha.12w38b/src/main/java/net/lostluma/server_stats/mixin/client/ServerPlayNetworkHandlerMixin.java
+++ module/versions/1.3.2-to-1.4.0-alpha.12w38b/src/main/java/net/lostluma/server_stats/mixin/client/ServerPlayNetworkHandlerMixin.java
@@ -1,12 +1,11 @@
 package net.lostluma.server_stats.mixin.client;
 
-import net.minecraft.network.packet.MenuClickSlotPacket;
+import net.minecraft.network.packet.CloseMenuPacket;
 import net.minecraft.server.entity.living.player.ServerPlayerEntity;
 import net.minecraft.server.network.handler.ServerPlayNetworkHandler;
 import net.minecraft.stat.achievement.Achievements;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,20 +15,12 @@ public class ServerPlayNetworkHandlerMixin {
 	@Shadow
 	private ServerPlayerEntity player;
 
-	@Unique
-	private int server_stats$menuId = -1;
-
 	/**
 	 * Award the open inventory achievement server-side.
 	 * This is not the most accurate, however will roughly track it correctly.
 	 */
-	@Inject(method = "handleMenuClickSlot", at = @At("HEAD"))
-	private void handleMenuClickSlot(MenuClickSlotPacket packet, CallbackInfo callbackInfo) {
-		if (packet.menuId == this.server_stats$menuId) {
-			return;
-		}
-
-		this.server_stats$menuId = packet.menuId;
+	@Inject(method = "handleCloseMenu", at = @At("HEAD"))
+	private void handleCloseMenu(CloseMenuPacket packet, CallbackInfo callbackInfo) {
 		this.player.incrementStat(Achievements.OPEN_INVENTORY);
 	}
 }
--- module/versions/1.5.0-alpha.13w02a-to-1.5.2/src/main/java/net/lostluma/server_stats/mixin/client/ServerPlayNetworkHandlerMixin.java
+++ module/versions/1.5.0-alpha.13w02a-to-1.5.2/src/main/java/net/lostluma/server_stats/mixin/client/ServerPlayNetworkHandlerMixin.java
@@ -1,12 +1,11 @@
 package net.lostluma.server_stats.mixin.client;
 
-import net.minecraft.network.packet.MenuClickSlotPacket;
+import net.minecraft.network.packet.CloseMenuPacket;
 import net.minecraft.server.entity.living.player.ServerPlayerEntity;
 import net.minecraft.server.network.handler.ServerPlayNetworkHandler;
 import net.minecraft.stat.achievement.Achievements;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,22 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
 	@Shadow
-	private ServerPlayerEntity player;
-
-	@Unique
-	private int server_stats$menuId = -1;
+	public ServerPlayerEntity player;
 
 	/**
 	 * Award the open inventory achievement server-side.
 	 * This is not the most accurate, however will roughly track it correctly.
 	 */
-	@Inject(method = "handleMenuClickSlot", at = @At("HEAD"))
-	private void handleMenuClickSlot(MenuClickSlotPacket packet, CallbackInfo callbackInfo) {
-		if (packet.menuId == this.server_stats$menuId) {
-			return;
-		}
-
-		this.server_stats$menuId = packet.menuId;
+	@Inject(method = "handleCloseMenu", at = @At("HEAD"))
+	private void handleCloseMenu(CloseMenuPacket packet, CallbackInfo callbackInfo) {
 		this.player.incrementStat(Achievements.OPEN_INVENTORY);
 	}
 }
