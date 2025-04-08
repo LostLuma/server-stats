0db8465f6d617b504539984ba72b51e52df638b1
LostLuma <lilly@lostluma.net>
2025-04-08 13:43:22 +0200

    Remove debug print statements
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- module/network/osl/client/src/main/java/net/lostluma/server_stats/network/client/Networking.java
+++ module/network/osl/client/src/main/java/net/lostluma/server_stats/network/client/Networking.java
@@ -10,15 +10,11 @@ public class Networking implements ClientModInitializer {
 	@Override
 	public void initClient() {
 		ClientPlayNetworking.registerListener(Constants.STATS_PACKET_SMALL_CHANNEL, SyncStatsPacket::new, ((minecraft, handler, payload) -> {
-			System.out.println("received small stats packet");
-			System.out.println("contains " + payload.data().size() + " items");
 			Minecraft.INSTANCE.statHandler.server_stats$persist(payload.data(), true);
 			return true;
 		}));
 
 		ClientPlayNetworking.registerListener(Constants.STATS_PACKET_LARGE_CHANNEL, SyncStatsPacket::new, ((minecraft, handler, payload) -> {
-			System.out.println("received large stats packet");
-			System.out.println("contains " + payload.data().size() + " items");
 			Minecraft.INSTANCE.statHandler.server_stats$persist(payload.data(), false);
 			return true;
 		}));
