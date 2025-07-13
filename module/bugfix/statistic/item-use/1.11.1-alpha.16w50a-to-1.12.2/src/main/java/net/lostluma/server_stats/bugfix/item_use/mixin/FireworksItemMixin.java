0381291cd336c1dffc85fdb037e6dd7982d63f5f
LostLuma <lilly@lostluma.net>
2025-07-13 18:58:11 +0200

    Add Firework Rocket usage bugfix to Item Usage module
    This fixes [MC-111534](https://bugs.mojang.com/browse/MC-111534).
    
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

---
+++ module/bugfix/statistic/item-use/1.11.1-alpha.16w50a-to-1.12.2/src/main/java/net/lostluma/server_stats/bugfix/item_use/mixin/FireworksItemMixin.java
@@ -0,0 +1,27 @@
+package net.lostluma.server_stats.bugfix.item_use.mixin;
+
+import net.minecraft.entity.living.player.PlayerEntity;
+import net.minecraft.item.FireworksItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FireworksItem.class)
+public class FireworksItemMixin extends Item {
+	@Inject(method = "startUsing", at = @At("RETURN"))
+	private void startUsing(World world, PlayerEntity player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> callbackInfo) {
+		InteractionResultHolder<ItemStack> result = callbackInfo.getReturnValue();
+
+		if (result.getResult().equals(InteractionResult.SUCCESS)) {
+			player.incrementStat(Stats.itemUsed(this));
+		}
+	}
+}
--- module/bugfix/statistic/item-use/README.md
+++ module/bugfix/statistic/item-use/README.md
@@ -1,3 +1,3 @@
 # Item Use
 
-This module fixes [MC-31047](https://bugs.mojang.com/browse/MC-31047).
+This module fixes [MC-31047](https://bugs.mojang.com/browse/MC-31047) and [MC-111534](https://bugs.mojang.com/browse/MC-111534).
