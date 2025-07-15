1effa38da5490a8822bf90baabfb2819b72ea93a
LostLuma <lilly@lostluma.net>
2025-07-15 21:58:26 +0200

    Fix combat statistics being created for non-living entities
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- module/statistic/combat/1.0.0-beta.1.8.0-to-1.1.0-alpha.11w48a/src/main/java/net/lostluma/server_stats/statistic/combat/mixin/common/EntitiesMixin.java
+++ module/statistic/combat/1.0.0-beta.1.8.0-to-1.1.0-alpha.11w48a/src/main/java/net/lostluma/server_stats/statistic/combat/mixin/common/EntitiesMixin.java
@@ -2,6 +2,7 @@ package net.lostluma.server_stats.statistic.combat.mixin.common;
 
 import net.lostluma.server_stats.api.statistic.ServerStatistic;
 import net.minecraft.entity.Entities;
+import net.minecraft.entity.living.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EntitiesMixin {
 	@Inject(method = "register", at = @At("TAIL"))
 	private static void registerWithSpawnEgg(Class<?> type, String key, int id, CallbackInfo callbackInfo) {
-		ServerStatistic.of("minecraft", "killEntity." + key).build();
-		ServerStatistic.of("minecraft", "entityKilledBy." + key).build();
+		if (LivingEntity.class.isAssignableFrom(type)) {
+			ServerStatistic.of("minecraft", "killEntity." + key).build();
+			ServerStatistic.of("minecraft", "entityKilledBy." + key).build();
+		}
 	}
 }
