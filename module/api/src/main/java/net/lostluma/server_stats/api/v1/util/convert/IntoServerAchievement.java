bab09ce5b0fd1442b3567e16bab644ae7cc56ddd
LostLuma <lilly@lostluma.net>
2025-07-20 13:56:52 +0200

    Improve Into* type docstrings
    Now documents how to convert to the Into type.
    
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- module/api/src/main/java/net/lostluma/server_stats/api/v1/util/convert/IntoServerAchievement.java
+++ module/api/src/main/java/net/lostluma/server_stats/api/v1/util/convert/IntoServerAchievement.java
@@ -1,8 +1,10 @@
 package net.lostluma.server_stats.api.v1.util.convert;
 
 /**
- * An object which may be converted into a {@code ServerAchievement}.
+ * A type which may be converted into a {@code ServerAchievement}.
  * <br>
- * This interface is implemented on {@code AchievementStat} via injected interfaces.
+ * To do so call {@code ServerAchievement::from} with the marked object.
+ * <br><br>
+ * On applicable Minecraft versions {@code AchievementStat} and {@code Advancement} implement this interface.
  */
 public interface IntoServerAchievement {}
--- module/api/src/main/java/net/lostluma/server_stats/api/v1/util/convert/IntoServerStatistic.java
+++ module/api/src/main/java/net/lostluma/server_stats/api/v1/util/convert/IntoServerStatistic.java
@@ -1,8 +1,10 @@
 package net.lostluma.server_stats.api.v1.util.convert;
 
 /**
- * An object which may be converted into a {@code ServerStatistic}.
+ * A type which may be converted into a {@code ServerStatistic}.
  * <br>
- * This interface is implemented on {@code Stat} via injected interfaces.
+ * To do so call {@code ServerStatistic::from} with the marked object.
+ * <br><br>
+ * On applicable Minecraft versions {@code Stat} implements this interface.
  */
 public interface IntoServerStatistic {}
