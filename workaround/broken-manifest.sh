6b6bb2b6c622f406d5763741f5a2bb435bafdc0a
LostLuma <lilly@lostluma.net>
2025-07-30 11:17:50 +0200

    Create directories before applying workaround
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- workaround/broken-manifest.sh
+++ workaround/broken-manifest.sh
@@ -2,6 +2,11 @@
 
 # The Minecraft Jar URLs in the original manifests no longer work
 # The manifests shipped in the workaround directory link to Omniarchive instead
+mkdir -p ~/.gradle/caches/fabric-loom/a1.0.6
 cp workaround/manifest/a1.0.6.json ~/.gradle/caches/fabric-loom/a1.0.6/skyrising_minecraft_info.json
+
+mkdir -p ~/.gradle/caches/fabric-loom/a1.0.10
 cp workaround/manifest/a1.0.10.json ~/.gradle/caches/fabric-loom/a1.0.10/skyrising_minecraft_info.json
+
+mkdir -p ~/.gradle/caches/fabric-loom/a1.2.0
 cp workaround/manifest/a1.2.0.json ~/.gradle/caches/fabric-loom/a1.2.0/skyrising_minecraft_info.json
